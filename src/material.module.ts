1549901a12267e0e8e7f907b10febec1e2243821
maxgerashenko <mgerashchenko@gmail.com>
2023-03-30 12:28:33 -0400

    3 files modified

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -11,6 +12,7 @@ const modules = [
   MatCardModule,
   MatSlideToggleModule,
   MatProgressBarModule,
+  MatFormFieldModule,
 ];
 
 @NgModule({
