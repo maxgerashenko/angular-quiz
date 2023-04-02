badc04cf45521fbd6820f02ea5ef77783dae0741
maxgerashenko <mgerashchenko@gmail.com>
2023-04-02 14:22:18 -0400

    1 file added

---
+++ src/sources/6_scalability.ts
@@ -0,0 +1,160 @@
+export const scalabilityQuiz = {
+  id: '1680459352775',
+  summary: `
+  Learning Notes: Scalability
+Scalability refers to the ability of a system to handle an increasing amount of workload without compromising its performance. It is an important consideration for any system or application that is expected to grow in usage over time.
+
+Dimensions of Scalability
+There are different dimensions of scalability, including:
+
+Size: Refers to the ability to handle an increasing number of users or resources. As the number of users or resources grows, the system should be able to handle the increased workload without slowing down or failing.
+
+Administrative: Refers to the ability to handle multiple users with the same load over all. This is important for systems that are used by many users at the same time, such as social media platforms.
+
+Geographical: Refers to the ability to service a broad geographical area. This is important for systems that are used by users in different locations, such as e-commerce platforms.
+
+Options for Scalability
+There are two main options for achieving scalability:
+
+Vertical scalability (scaling up): Involves adding more resources to a single machine, such as adding more CPU, memory, or storage. This approach can be limited by the physical constraints of the machine and can be expensive.
+
+Horizontal scalability (scaling out): Involves adding more machines to the system, such as adding more servers. This approach is more flexible and can handle larger workloads, but requires more coordination and management of the multiple machines.
+
+In summary, scalability is an important consideration for any system or application that is expected to grow in usage over time. Understanding the different dimensions of scalability and the options for achieving it can help in designing and managing scalable systems.
+  `,
+  title: 'Scalability Quiz',
+  questionList: [
+    {
+      text: 'What does scalability refer to?',
+      optionList: [
+        'The ability of a system to handle a decreasing amount of workload without compromising performance',
+        'The ability of a system to handle an increasing amount of workload without compromising performance',
+        'The ability of a system to handle a fixed amount of workload without compromising performance',
+        'The ability of a system to handle a varying amount of workload without compromising performance',
+      ],
+      answer: 'b',
+      description:
+        'Scalability refers to the ability of a system to handle an increasing amount of workload without compromising its performance.',
+    },
+    {
+      text: 'What is the size dimension of scalability?',
+      optionList: [
+        'Refers to the ability to handle multiple users with the same load over all',
+        'Refers to the ability to service a broad geographical area',
+        'Refers to the ability to handle an increasing number of users or resources',
+        'None of the above',
+      ],
+      answer: 'c',
+      description:
+        'Size dimension of scalability refers to the ability to handle an increasing number of users or resources.',
+    },
+    {
+      text: 'What is the difference between vertical scalability and horizontal scalability?',
+      optionList: [
+        'Vertical scalability involves adding more servers, while horizontal scalability involves adding more resources to a single machine',
+        'Vertical scalability involves adding more resources to a single machine, while horizontal scalability involves adding more servers',
+        'Vertical scalability is more expensive than horizontal scalability',
+        'None of the above',
+      ],
+      answer: 'b',
+      description:
+        'Vertical scalability involves adding more resources to a single machine, such as adding more CPU, memory, or storage. Horizontal scalability involves adding more machines to the system, such as adding more servers.',
+    },
+    {
+      text: 'What is the administrative dimension of scalability?',
+      optionList: [
+        'Refers to the ability to handle an increasing number of users or resources',
+        'Refers to the ability to service a broad geographical area',
+        'Refers to the ability to handle multiple users with the same load over all',
+        'None of the above',
+      ],
+      answer: 'c',
+      description:
+        'Administrative dimension of scalability refers to the ability to handle multiple users with the same load over all. This is important for systems that are used by many users at the same time, such as social media platforms.',
+    },
+    {
+      text: 'What are some common methods for achieving horizontal scalability?',
+      optionList: [
+        'Adding more resources to a single machine',
+        'Adding more servers to the system',
+        'Using load balancers to distribute traffic',
+        'All of the above',
+      ],
+      answer: 'c',
+      description:
+        'Common methods for achieving horizontal scalability include adding more servers to the system and using load balancers to distribute traffic.',
+    },
+    {
+      text: 'What are some common methods for achieving vertical scalability?',
+      optionList: [
+        'Adding more resources to a single machine',
+        'Adding more servers to the system',
+        'Using load balancers to distribute traffic',
+        'All of the above',
+      ],
+      answer: 'a',
+      description:
+        'Common methods for achieving vertical scalability include adding more resources to a single machine, such as adding more CPU, memory, or storage.',
+    },
+    {
+      text: 'What are some potential limitations of vertical scalability?',
+      optionList: [
+        'It can be expensive',
+        'It can be limited by the physical constraints of the machine',
+        'It can be difficult to manage and coordinate multiple machines',
+        'None of the above',
+      ],
+      answer: 'b',
+      description:
+        'Vertical scalability can be limited by the physical constraints of the machine, such as the maximum number of CPUs or amount of memory that can be added. This can make it difficult to scale beyond a certain point.',
+    },
+    {
+      text: 'What is the difference between scaling up and scaling out?',
+      optionList: [
+        'Scaling up involves adding more machines to the system, while scaling out involves adding more resources to a single machine',
+        'Scaling up involves adding more resources to a single machine, while scaling out involves adding more machines to the system',
+        'Scaling up is more expensive than scaling out',
+        'None of the above',
+      ],
+      answer: 'b',
+      description:
+        'Scaling up involves adding more resources to a single machine, such as adding more CPU, memory, or storage. Scaling out involves adding more machines to the system, such as adding more servers.',
+    },
+    {
+      text: 'What is the difference between stateless and stateful systems in terms of scalability?',
+      optionList: [
+        'Stateless systems are more scalable than stateful systems',
+        'Stateful systems are more scalable than stateless systems',
+        'There is no difference in scalability between stateless and stateful systems',
+        'It depends on the specific implementation',
+      ],
+      answer: 'a',
+      description:
+        "Stateless systems are more scalable than stateful systems because they don't maintain any state or data between requests. This makes it easier to distribute requests across multiple machines without worrying about synchronization or consistency issues.",
+    },
+    {
+      text: 'What is sharding?',
+      optionList: [
+        'A method for achieving vertical scalability',
+        'A method for achieving horizontal scalability',
+        'A method for achieving both vertical and horizontal scalability',
+        'None of the above',
+      ],
+      answer: 'b',
+      description:
+        'Sharding is a method for achieving horizontal scalability by partitioning data across multiple machines. Each machine is responsible for a subset of the data, which allows for better performance and scalability.',
+    },
+    {
+      text: 'What are some challenges of achieving scalability?',
+      optionList: [
+        'Maintaining data consistency across multiple machines',
+        'Managing and coordinating multiple machines',
+        'Ensuring high availability and fault tolerance',
+        'All of the above',
+      ],
+      answer: 'd',
+      description:
+        'Achieving scalability can be challenging due to various factors such as maintaining data consistency across multiple machines, managing and coordinating multiple machines, and ensuring high availability and fault tolerance.',
+    },
+  ],
+};
