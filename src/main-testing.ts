60a4ff168cc4e0fb2c43fb7749ef2aafad6b1a47
maxgerashenko <mgerashchenko@gmail.com>
2023-04-02 09:52:11 -0400

    1 file modified, 3 added

---
+++ src/main-testing.ts
@@ -0,0 +1,43 @@
+'./global-jasmine';
+import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+import 'jasmine-core/lib/jasmine-core/boot.js';
+
+import './polyfills';
+
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+
+// Requires 'zone.js/dist/proxy.js' and 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// stuff to test
+import './app/app.component.spec.ts';
+
+jasmine.getEnv().configure({ random: false });
+bootstrap();
+
+function bootstrap() {
+  if (window.jasmineRef) {
+    location.reload();
+    return;
+  } else {
+    window.onload();
+    window.jasmineRef = jasmine.getEnv();
+  }
+
+  // First, initialize the Angular testing environment.
+  getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+}
