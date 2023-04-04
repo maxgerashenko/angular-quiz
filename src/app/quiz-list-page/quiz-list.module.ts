cbbd77be95786689880fa6acfb5eb83aa89c8c1e
maxgerashenko <mgerashchenko@gmail.com>
2023-04-03 21:21:52 -0400

    1 file modified

--- src/app/quiz-list-page/quiz-list.module.ts
+++ src/app/quiz-list-page/quiz-list.module.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 import { QuizListPageComponent } from './quiz-list-page.component';
 import { RouterModule } from '@angular/router';
 import { PipesModule } from '../pipes/pipes.module';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-
-
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressbarModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule, 
+    RouterModule,
     PipesModule,
     MatListModule,
     MatCardModule,
+    MatProgressbarModule,
   ],
   declarations: [QuizListPageComponent],
   exports: [],
