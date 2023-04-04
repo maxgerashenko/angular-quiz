3fbfe7f696025d62cc08e3a8657fcdab410330e6
maxgerashenko <mgerashchenko@gmail.com>
2023-04-04 15:26:20 -0400

    1 file modified

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,17 +1,18 @@
-
-
 export interface ObjectType<T> {
-    [key:string]: T
-};
-
-export function assert(value:any, message:string) {
-    if (!value) {
-        const valueName = Object.keys({value})[0];
-        throw (`Assertion Error: ${valueName} ${message}` || `Assertion ${valueName} failed`);
-    }
+  [key: string]: T;
 }
 
-export function castExists<T>(value: T, text:string) {
-    assert(value, text); 
-    return value;
-    }
+export function assert(value: any, message: string) {
+  if (value == null) {
+    const valueName = Object.keys({ value })[0];
+    throw (
+      `Assertion Error: ${valueName} ${message}` ||
+      `Assertion ${valueName} failed`
+    );
+  }
+}
+
+export function castExists<T>(value: T, text: string) {
+  assert(value, text);
+  return value;
+}
