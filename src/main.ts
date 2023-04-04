01e0457afed23a1dbc15e0d7cd24f55b32935cf0
maxgerashenko <mgerashchenko@gmail.com>
2023-04-03 21:03:29 -0400

    src deleted

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { MainModule } from './main.module';
-import 'zone.js/dist/zone'; // importatnt for bootstrap
-
-platformBrowserDynamic().bootstrapModule(MainModule); // importatnt for bootstrap
--- src/start-page/start-page.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StartPageComponent } from './start-page.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [StartPageComponent],
-})
-export class StartPageModule {}
