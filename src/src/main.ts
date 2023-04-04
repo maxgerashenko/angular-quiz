6e491d16b0c10e72a5388f2a3f495ab44acdece4
Max Gerashenko <mgerashchenko@gmail.com>
2023-04-03 21:05:13 -0400

    Add files via upload

---
+++ src/src/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MainModule } from './main/main.module';
+
+
+platformBrowserDynamic().bootstrapModule(MainModule)
+  .catch(err => console.error(err));
