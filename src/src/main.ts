a477d72ade71d5f707e2dfdc2ab08ea8a667b39b
Max Gerashenko <mgerashchenko@gmail.com>
2023-04-03 21:07:13 -0400

    delete again

--- src/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { MainModule } from './main/main.module';
-
-
-platformBrowserDynamic().bootstrapModule(MainModule)
-  .catch(err => console.error(err));
