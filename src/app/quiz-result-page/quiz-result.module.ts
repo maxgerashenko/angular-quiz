7088a5e14e7b8edaed93538bd2c71b889252e4cd
maxgerashenko <mgerashchenko@gmail.com>
2023-04-04 19:52:47 -0400

    3 files modified

--- src/app/quiz-result-page/quiz-result.module.ts
+++ src/app/quiz-result-page/quiz-result.module.ts
@@ -2,25 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuizResultPageComponent } from './quiz-result-page.component';
 import { PipesModule } from '../pipes/pipes.module';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
-    CommonModule, 
+    CommonModule,
     PipesModule,
     MatButtonModule,
     MatCardModule,
     MatListModule,
     MatProgressBarModule,
     MatFormFieldModule,
+    MatInputModule,
   ],
-  declarations: [
-    QuizResultPageComponent,
-  ],
+  declarations: [QuizResultPageComponent],
   exports: [QuizResultPageComponent],
 })
 export class QuizResultsModule {}
