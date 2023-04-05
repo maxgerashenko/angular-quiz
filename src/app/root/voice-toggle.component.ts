968e519b3da799c449a0df7b6c3e5ed8b75dfacd
maxgerashenko <mgerashchenko@gmail.com>
2023-04-05 10:59:21 -0400

    1 file modified

--- src/app/root/voice-toggle.component.ts
+++ src/app/root/voice-toggle.component.ts
@@ -1,37 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { VoiceService } from '../services/voice.service';
 
+export interface ToggleButtonOptions<T> {
+  isActive: boolean;
+  title: string;
+  subtitle?: string;
+  icon: string;
+  value: T;
+}
+
+export interface VoiveSettings {
+  isVoiceOverOn: boolean;
+  isVoiveOverMessagesOn: boolean;
+}
+
+const VOICE_OVER_OPTIONS = [
+  {
+    isActive: true,
+    title: 'Read',
+    subtitle: 'Question & Result',
+    icon: 'volume_up',
+    value: {
+      isVoiceOverOn: true,
+      isVoiveOverMessagesOn: true,
+    },
+  },
+  {
+    isActive: true,
+    title: 'Read',
+    subtitle: 'Question',
+    icon: 'volume_down',
+    value: {
+      isVoiceOverOn: true,
+      isVoiveOverMessagesOn: false,
+    },
+  },
+  {
+    isActive: false,
+    title: 'Turn Off',
+    icon: 'volume_off',
+    value: {
+      isVoiceOverOn: false,
+      isVoiveOverMessagesOn: false,
+    },
+  },
+];
+
 @Component({
   selector: 'app-voice-toggle',
   templateUrl: './voice-toggle.component.html',
   styleUrls: ['./voice-toggle.component.css'],
 })
 export class VoiceToggleComponent implements OnInit {
-  voiceOverOptions = [
-    {
-      isActive: true,
-      title: 'Read',
-      subtitle: 'Question & Result',
-      icon: 'volume_up',
-      isVoiceOverOn: true,
-      isVoiveOverMessagesOn: true,
-    },
-    {
-      isActive: true,
-      title: 'Read',
-      subtitle: 'Question',
-      icon: 'volume_down',
-      isVoiceOverOn: true,
-      isVoiveOverMessagesOn: false,
-    },
-    {
-      isActive: false,
-      title: 'Turn Off',
-      icon: 'volume_off',
-      isVoiceOverOn: false,
-      isVoiveOverMessagesOn: false,
-    },
-  ];
+  voiceOverOptions: ToggleButtonOptions<VoiveSettings>[] = VOICE_OVER_OPTIONS;
   voiceOverIndex = 0;
   voiceOverSettings = this.voiceOverOptions[this.voiceOverIndex];
 
@@ -63,7 +84,8 @@ export class VoiceToggleComponent implements OnInit {
 
   updateVoiceOver() {
     this.setVoiceOver((this.voiceOverIndex + 1) % this.voiceOverOptions.length);
-    const { isVoiceOverOn, isVoiveOverMessagesOn } = this.voiceOverSettings;
+    const { isVoiceOverOn, isVoiveOverMessagesOn } =
+      this.voiceOverSettings.value;
     this.voiceService.updateSettigns(isVoiceOverOn, isVoiveOverMessagesOn);
   }
 }
