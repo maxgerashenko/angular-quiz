03eaeac7936c394b692473ae60384fac2b336eea
maxgerashenko <mgerashchenko@gmail.com>
2023-04-05 09:48:43 -0400

    6 files modified

--- src/app/root/courses-nav.component.ts
+++ src/app/root/courses-nav.component.ts
@@ -2,8 +2,6 @@ import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SourceService } from '../services/source.service';
 import { MenuService } from '../services/menu.service';
-import { VoiceService } from '../services/voice.service';
-// import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-courses-nav',
@@ -13,64 +11,12 @@ import { VoiceService } from '../services/voice.service';
 })
 export class CoursesNavComponent {
   courses = this.sourceService.getCourseTileList();
-  voiceOverOptions = [
-    {
-      isActive: true,
-      title: 'Read',
-      subtitle: 'Question & Result',
-      icon: 'volume_up',
-      isVoiceOverOn: true,
-      isVoiveOverMessagesOn: true,
-    },
-    {
-      isActive: true,
-      title: 'Read',
-      subtitle: 'Question',
-      icon: 'volume_down',
-      isVoiceOverOn: true,
-      isVoiveOverMessagesOn: false,
-    },
-    {
-      isActive: false,
-      title: 'Turn Off',
-      icon: 'volume_off',
-      isVoiceOverOn: false,
-      isVoiveOverMessagesOn: false,
-    },
-  ];
-  voiceOverIndex = 0;
-  voiceOverSettings = this.voiceOverOptions[this.voiceOverIndex];
 
   constructor(
     private router: Router,
     private sourceService: SourceService,
-    private voiceService: VoiceService,
     public menuService: MenuService
-  ) {
-    const { isVoiceOverOn, isVoiceOverMessagesOn } =
-      this.voiceService.getSettings();
-
-    if (isVoiceOverOn && isVoiceOverMessagesOn) {
-      this.setVoiceOver(0);
-    }
-    if (isVoiceOverOn && !isVoiceOverMessagesOn) {
-      this.setVoiceOver(1);
-    }
-    if (!isVoiceOverOn && !isVoiceOverMessagesOn) {
-      this.setVoiceOver(2);
-    }
-  }
-
-  private setVoiceOver(voiceOverIndex) {
-    this.voiceOverIndex = voiceOverIndex;
-    this.voiceOverSettings = this.voiceOverOptions[this.voiceOverIndex];
-  }
-
-  updateVoiceOver() {
-    this.setVoiceOver((this.voiceOverIndex + 1) % this.voiceOverOptions.length);
-    const { isVoiceOverOn, isVoiveOverMessagesOn } = this.voiceOverSettings;
-    this.voiceService.updateSettigns(isVoiceOverOn, isVoiveOverMessagesOn);
-  }
+  ) {}
 
   openCourse(courseId: string) {
     this.router.navigate(['/course'], { queryParams: { id: courseId } });
--- src/app/root/voice-toggle.component.ts
+++ src/app/root/voice-toggle.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { VoiceService } from '../services/voice.service';
 
 @Component({
   selector: 'app-voice-toggle',
   templateUrl: './voice-toggle.component.html',
-  styleUrls: ['./voice-toggle.component.css']
+  styleUrls: ['./voice-toggle.component.css'],
 })
 export class VoiceToggleComponent implements OnInit {
+  voiceOverOptions = [
+    {
+      isActive: true,
+      title: 'Read',
+      subtitle: 'Question & Result',
+      icon: 'volume_up',
+      isVoiceOverOn: true,
+      isVoiveOverMessagesOn: true,
+    },
+    {
+      isActive: true,
+      title: 'Read',
+      subtitle: 'Question',
+      icon: 'volume_down',
+      isVoiceOverOn: true,
+      isVoiveOverMessagesOn: false,
+    },
+    {
+      isActive: false,
+      title: 'Turn Off',
+      icon: 'volume_off',
+      isVoiceOverOn: false,
+      isVoiveOverMessagesOn: false,
+    },
+  ];
+  voiceOverIndex = 0;
+  voiceOverSettings = this.voiceOverOptions[this.voiceOverIndex];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private voiceService: VoiceService) {
+    this.init();
   }
 
+  ngOnInit() {}
+
+  private init() {
+    const { isVoiceOverOn, isVoiceOverMessagesOn } =
+      this.voiceService.getSettings();
+
+    if (isVoiceOverOn && isVoiceOverMessagesOn) {
+      this.setVoiceOver(0);
+    }
+    if (isVoiceOverOn && !isVoiceOverMessagesOn) {
+      this.setVoiceOver(1);
+    }
+    if (!isVoiceOverOn && !isVoiceOverMessagesOn) {
+      this.setVoiceOver(2);
+    }
+  }
+
+  private setVoiceOver(voiceOverIndex) {
+    this.voiceOverIndex = voiceOverIndex;
+    this.voiceOverSettings = this.voiceOverOptions[this.voiceOverIndex];
+  }
+
+  updateVoiceOver() {
+    this.setVoiceOver((this.voiceOverIndex + 1) % this.voiceOverOptions.length);
+    const { isVoiceOverOn, isVoiveOverMessagesOn } = this.voiceOverSettings;
+    this.voiceService.updateSettigns(isVoiceOverOn, isVoiveOverMessagesOn);
+  }
 }
