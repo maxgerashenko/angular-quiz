9ca6c83b1bc67a716239393f1a9af5cc05ffefe5
maxgerashenko <mgerashchenko@gmail.com>
2023-04-05 01:35:17 -0400

    1 file modified

--- src/app/services/tmp.ts
+++ src/app/services/tmp.ts
@@ -2,14 +2,42 @@ import { Course } from './interfaces';
 
 export const TMP_COURSES_LIST: Course[] = [
   {
-    id: '1',
+    id: '0',
     title: 'Angular',
     quizzesList: [
       {
-        id: '1',
+        id: '0',
+        title: 'Introduction to Angular',
+        courseId: '0',
+        courseTitle: 'Angular',
+        questionsList: [
+          {
+            title: 'What language does angular use?',
+            optionsList: ['Java', 'JavaScript', 'Python', 'C#'],
+            answer: 'b',
+            description:
+              'Java is not the correct answer because Angular is a JavaScript framework and uses JavaScript as its language, not Java. The two languages have different syntax, features, and libraries.',
+          },
+          {
+            title: 'What language does angular use 2?',
+            optionsList: ['Java', 'JavaScript', 'Python', 'C#'],
+            answer: 'b',
+            description:
+              'Java is not the correct answer because Angular is a JavaScript framework and uses JavaScript as its language, not Java. The two languages have different syntax, features, and libraries.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '1',
+    title: 'Angular 2',
+    quizzesList: [
+      {
+        id: '0',
         title: 'Introduction to Angular',
         courseId: '1',
-        courseTitle: 'Angular',
+        courseTitle: 'Angular 2',
         questionsList: [
           {
             title: 'What language does angular use?',
