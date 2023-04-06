96f99b90b9d736a9b098b07098789912fdc1fe4a
maxgerashenko <mgerashchenko@gmail.com>
2023-04-06 10:47:08 -0400

    1 file modified

--- data/distributedMonitoring.ts
+++ data/distributedMonitoring.ts
@@ -7,6 +7,120 @@ export const distributedMonitoring: Quiz = {
   title: 'Distributed Monitoring',
   questionsList: [
     {
+      title:
+        'What is the purpose of the rules database in the monitoring system?',
+      optionsList: [
+        'To store configuration data for the monitoring system',
+        'To store metrics for analysis',
+        'To store rules for alerting',
+        'To store log data for debugging purposes',
+      ],
+      answer: 'c',
+      description:
+        'The rules database is used to store rules that are used to generate alerts based on the collected metrics.',
+    },
+    {
+      title:
+        'What is the purpose of the querying service in the monitoring system?',
+      optionsList: [
+        'To retrieve data from the monitored systems',
+        'To store metrics for analysis',
+        'To query the time-series databases for information',
+        'To generate alerts based on collected metrics',
+      ],
+      answer: 'c',
+      description:
+        'The querying service is used to query the time-series databases for information and return relevant data to the user.',
+    },
+    {
+      title:
+        'What is the role of the service discoverer in the monitoring system?',
+      optionsList: [
+        'To retrieve data from the monitored systems',
+        'To store metrics for analysis',
+        'To generate alerts based on collected metrics',
+        'To identify what systems to monitor',
+      ],
+      answer: 'd',
+      description:
+        'The service discoverer identifies what systems to monitor in the distributed system, such as Kubernetes clusters or other infrastructure.',
+    },
+    {
+      title: 'What is the purpose of the dashboard in the monitoring system?',
+      optionsList: [
+        'To store metrics for analysis',
+        'To generate alerts based on collected metrics',
+        'To query the time-series databases for information',
+        'To view collected metrics in a user-friendly format',
+      ],
+      answer: 'd',
+      description:
+        'The dashboard is used to view collected metrics in a user-friendly format, allowing users to easily monitor the performance of the distributed system.',
+    },
+    {
+      title:
+        'What are the benefits of a push strategy in the monitoring system?',
+      optionsList: [
+        'Reduced latency',
+        'Reduced network traffic',
+        'Scalability',
+        'Control over data',
+      ],
+      answer: 'a',
+      description:
+        'A push strategy can reduce latency in the monitoring system, allowing for real-time updates and notifications.',
+    },
+    {
+      title: 'What are the cons of a pull strategy in the monitoring system?',
+      optionsList: [
+        'High volume of notifications',
+        'Increased network traffic',
+        'Incomplete data',
+        'Resource usage on the monitored systems',
+      ],
+      answer: 'c',
+      description:
+        'With a pull strategy, there can be a delay between when data is requested and when it is received, resulting in potentially incomplete data for time-sensitive applications.',
+    },
+    {
+      title:
+        'What is the purpose of the time-series databases in the monitoring system?',
+      optionsList: [
+        'To store configuration data for the monitoring system',
+        'To store metrics for analysis',
+        'To store rules for alerting',
+        'To store log data for debugging purposes',
+      ],
+      answer: 'b',
+      description:
+        'The time-series databases are used to store metrics that are collected from the monitored systems for further analysis.',
+    },
+    {
+      title:
+        'What is the purpose of the Blob storage in the monitoring system?',
+      optionsList: [
+        'To store rules for alerting',
+        'To store metrics for analysis',
+        'To store configuration data for the monitoring system',
+        'To store log data for debugging purposes',
+      ],
+      answer: 'b',
+      description:
+        'The Blob storage is used to store metrics that are collected from the monitored systems.',
+    },
+    {
+      title: 'What is the role of the data collector in the monitoring system?',
+      optionsList: [
+        'It stores metrics in a time-series database',
+        'It sends alerts to the Alert Manager',
+        'It retrieves data from the monitored systems',
+        'It queries the time-series database for information',
+      ],
+      answer: 'c',
+      description:
+        'The data collector retrieves data from the monitored systems and sends it to the monitoring system for analysis and storage.',
+    },
+    {
       title: 'What are the two types of errors that can occur in a system?',
       optionsList: [
         '1xx errors and 2xx errors',
@@ -54,18 +168,6 @@ export const distributedMonitoring: Quiz = {
       answer: 'c',
       description:
         'A proactive approach aims to prevent downtime altogether, while a reactive approach involves fixing errors after they occur.',
-    },
-    {
-      title: 'What is important when persisting data in a system?',
-      optionsList: [
-        'Using the same database as everyone else',
-        'Storing data in spreadsheets',
-        'Using centralized in-memory metrics or a time-series database',
-        'Not persisting any data at all',
-      ],
-      answer: 'c',
-      description:
-        'Persisting data in centralized in-memory metrics or a time-series database is important for big data.',
     },
     {
       title: 'What is code instrumentation in a system?',
@@ -206,19 +308,6 @@ export const distributedMonitoring: Quiz = {
     },
     {
       title:
-        'What is the purpose of the Querying Service component in a server-side monitoring system?',
-      optionsList: [
-        'To identify services to monitor',
-        'To store data collected by the system',
-        'To define rules for triggering alerts',
-        'To provide a way to retrieve information from the logs and metrics collected by the data collection service',
-      ],
-      answer: 'd',
-      description:
-        'The Querying Service component provides a way to retrieve information from the logs and metrics collected by the data collection service.',
-    },
-    {
-      title:
         'What is the purpose of the Rules and Actions Database component in a server-side monitoring system?',
       optionsList: [
         'To identify services to monitor',
@@ -229,19 +318,6 @@ export const distributedMonitoring: Quiz = {
       answer: 'd',
       description:
         'The Rules and Actions Database component is responsible for defining rules for triggering alerts and actions to be taken in response.',
-    },
-    {
-      title:
-        'What is the purpose of the Service Discoverer component in a server-side monitoring system?',
-      optionsList: [
-        'To collect relevant metrics and set up alerts based on certain thresholds',
-        'To identify services to monitor',
-        "To provide a visual representation of the system's health",
-        'To provide a way to retrieve information from the logs and metrics collected by the data collection service',
-      ],
-      answer: 'b',
-      description:
-        'The Service Discoverer component identifies the services that need to be monitored and provides information about their location and status.',
     },
     {
       title:
