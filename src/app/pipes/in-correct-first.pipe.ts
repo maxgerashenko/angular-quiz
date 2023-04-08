4a8b1c1d70c9ebb212b0c0b51b80c0340dbe1e57
maxgerashenko <mgerashchenko@gmail.com>
2023-04-08 04:22:58 -0400

    2 files modified

--- src/app/pipes/in-correct-first.pipe.ts
+++ src/app/pipes/in-correct-first.pipe.ts
@@ -4,13 +4,33 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'inCorrectFirst',
 })
 export class inCorrectFirstPipe implements PipeTransform {
-  transform(questions: any[]): any[] {
-    const correctQuestions = questions.filter(
-      ({ answer, selectedValue }) => answer === selectedValue
-    );
-    const incorrectQuestions = questions.filter(
+  transform(items: any[], answer?, selectedValue?): any[] {
+    debugger;
+    if (typeof items[0] === 'string') {
+      if (selectedValue === answer) return items;
+      const incorrectOption = items.filter(
+        (option, index) => this.indexToLetter(index) == selectedValue
+      );
+      const correctOption = items.filter(
+        (option, index) => this.indexToLetter(index) === answer
+      );
+      const rest = items.filter((option, index) => {
+        const letter = this.indexToLetter(index);
+        return letter !== answer && letter !== selectedValue;
+      });
+      return [...incorrectOption, ...correctOption, ...rest];
+    }
+
+    const incorrectQuestions = items.filter(
       ({ answer, selectedValue }) => answer !== selectedValue
     );
+    const correctQuestions = items.filter(
+      ({ answer, selectedValue }) => answer === selectedValue
+    );
     return [...incorrectQuestions, ...correctQuestions];
+  }
+
+  private indexToLetter(index: number): string {
+    return String.fromCharCode(97 + index);
   }
 }
