fd395dd231fc8e59c93239e4cffc35b3e4d611d0
maxgerashenko <mgerashchenko@gmail.com>
2023-04-09 14:07:29 -0400

    1 file modified

--- src/app/quiz-list-page/quiz-list-page.component.ts
+++ src/app/quiz-list-page/quiz-list-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { Event as NavigationEvent, NavigationEnd } from '@angular/router';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SourceService } from '../services/source.service';
@@ -7,12 +7,15 @@ import { filter } from 'rxjs/operators';
 import { Subject, takeUntil } from 'rxjs';
 import { Course } from '../services/interfaces';
 import { MenuService } from '../services/menu.service';
+import { MatListItem } from '@angular/material/list';
 
 @Component({
   templateUrl: './quiz-list-page.component.html',
   styleUrls: ['./quiz-list-page.component.scss'],
 })
 export class QuizListPageComponent {
+  @ViewChildren(MatListItem) options: QueryList<MatListItem>;
+
   course?: Course;
   destroy = new Subject();
 
@@ -27,11 +30,6 @@ export class QuizListPageComponent {
     this.menuService.openMenu();
   }
 
-  updateCourse() {
-    let courseId = this.route.snapshot.queryParams['id'];
-    this.course = this.sourceService.getCourse(courseId);
-  }
-
   ngOnInit() {
     this.router.events
       .pipe(
@@ -41,6 +39,25 @@ export class QuizListPageComponent {
       .subscribe(() => {
         this.updateCourse();
       });
+  }
+
+  updateCourse() {
+    let courseId = this.route.snapshot.queryParams['id'];
+    this.course = this.sourceService.getCourse(courseId);
+  }
+
+  ngAfterViewInit() {
+    this.resetFoucs();
+  }
+
+  toggleMenu() {
+    setTimeout(() => {
+      this.resetFoucs();
+    }, 30);
+  }
+
+  resetFoucs() {
+    this.options.first._elementRef.nativeElement.focus();
   }
 
   openQuiz(courseId: string, quizId: string) {
